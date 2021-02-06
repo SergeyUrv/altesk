1e62425ea29071d8f7c678fe02a21ad90e7afd90
Sergey Гкмфсрум <sergey@urvachev.su>
2021-02-06 19:35:05 +0700

    Подготовка Django-проекта к деплою

--- wsgi.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-from os import environ
-from django.core.handlers.wsgi import WSGIHandler
-
-environ['DJANGO_SETTINGS_MODULE'] = environ.get(
-    'DJANGO_SETTINGS_MODULE', 'settings')
-
-application = WSGIHandler()
