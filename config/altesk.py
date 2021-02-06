357311f75af4fc364330f5187b57b7556f659cc4
Sergey Гкмфсрум <sergey@urvachev.su>
2021-02-06 23:39:53 +0700

    Подготовка Django-проекта к деплою, по урокам Omelchenko Michael (DJWOMS)

--- config/altesk.py
+++
@@ -1,7 +0,0 @@
-[program:altesk]
-command=/var/www/venv/bin/gunicorn altesksite.wsgi:application -c /var/www/altesk/altesksite/config/gunicorn.conf.py
-directory=/var/www/altesk/altesksite
-user=john
-autorestart=true
-redirect_stderr=true
-stdout_logfile = /var/www/altesk/logs/debug.log
